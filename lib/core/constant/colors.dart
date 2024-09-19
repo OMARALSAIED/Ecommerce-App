5b45b23307a2efb9085c3ab0bcbc4714a0f3775e
omar Alsaied <wwwomralsaied@gmaill.com>
2024-09-19 20:12:36 +0000

    catgeories model done

--- lib/core/constant/colors.dart
+++ lib/core/constant/colors.dart
@@ -4,7 +4,8 @@ class APPColors {
   static const Color gery = Color(0xff515151);
   // static const  Color primaryColor = Color(0xff5DB1DF);
   static const Color backgroundColor = Color(0xffF8F9FD);
-  static const Color primaryColor = Color(0xffF2786D);
+  static const Color primaryColor = Color.fromARGB(255, 185, 49, 37);
+  static const Color secondcolor = Color.fromARGB(255, 228, 61, 45);
   static const Color kBlue = Colors.blue;
   static const Color kBlack = Colors.black;
 }
--- lib/views/screens/Home/home.dart
+++ lib/views/screens/Home/home.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce/controller/home_Controller.dart';
+import 'package:ecommerce/core/constant/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -12,48 +13,83 @@ class HomePage extends StatelessWidget {
     HomeControllerImp controlle = Get.put(HomeControllerImp());
     return Scaffold(
         body: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
             child: ListView(
-      children: [
-        Container(
-          margin: EdgeInsets.only(top: 24),
-          padding: EdgeInsets.symmetric(horizontal: 18),
-          child: Row(
-            children: [
-              Expanded(
-                  child: TextFormField(
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    hintText: "Find Product",
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(20)),
-                    filled: true,
-                    fillColor: Color.fromARGB(34, 158, 158, 158)),
-              )),
-              const SizedBox(
-                width: 10,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(34, 158, 158, 158),
-                    borderRadius: BorderRadius.circular(15)),
-                width: 60,
-                height: 55,
-                padding: EdgeInsets.symmetric(vertical: 8),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.notifications_active_outlined,
-                    size: 30,
-                    color: Color.fromARGB(255, 126, 114, 114),
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 24),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search),
+                            hintText: "Find Product",
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(20)),
+                            filled: true,
+                            fillColor: Color.fromARGB(34, 158, 158, 158)),
+                      )),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: const Color.fromARGB(34, 158, 158, 158),
+                            borderRadius: BorderRadius.circular(15)),
+                        width: 60,
+                        height: 55,
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.notifications_active_outlined,
+                            size: 30,
+                            color: Color.fromARGB(255, 126, 114, 114),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-              )
-            ],
-          ),
-        ),
-        Center(child: Text("${controlle.userid}"))
-      ],
-    )));
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 15),
+                  child: Stack(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                              color: APPColors.primaryColor,
+                              borderRadius:
+                                  BorderRadiusDirectional.circular(20)),
+                          height: 150,
+                          child: const ListTile(
+                            title: Text(
+                              "A summer Suprise",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                            subtitle: Text(
+                              "Cahback 20%",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 30),
+                            ),
+                          )),
+                      Positioned(
+                        top: -25,
+                        right: -10,
+                        child: Container(
+                          height: 160,
+                          width: 160,
+                          decoration: BoxDecoration(
+                              color: APPColors.secondcolor,
+                              borderRadius: BorderRadius.circular(100)),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )));
   }
 }
